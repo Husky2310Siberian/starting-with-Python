d05ab72cc94c7b6d4370d0a3b20ef4b785f76f39
Siberian <vasileios.kritikos@aueb.gr>
2025-03-16 20:09:16 +0200

    find first and last index

---
+++ chapter2/07_find_first_last.py
@@ -0,0 +1,5 @@
+# Find the first and last colors from a given list
+
+color_list = ["red" , "orange" , "blue" , "dark"]
+
+print("In first position is %s and in last posistion is %s" % (color_list[0], color_list[-1]))

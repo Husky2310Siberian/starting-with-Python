12db8abd025e108191ca5a5aad69659d345b3834
Siberian <vasileios.kritikos@aueb.gr>
2025-03-17 15:38:10 +0200

    function demo

---
+++ chapter2/15_function_demo.py
@@ -0,0 +1,14 @@
+def say_hello(message):
+    """
+    Print message
+    Args:
+        message (str): message to print
+    """
+    # print("Hello World!")
+    print(message)
+
+def main():
+    say_hello("Hello again")
+
+if __name__ == "__main__":
+    main()

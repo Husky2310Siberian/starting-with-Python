4523703bcb1edb602e2899a2efd8753a1a7d045a
Siberian <vasileios.kritikos@aueb.gr>
2025-03-17 16:02:36 +0200

    module demo

---
+++ chapter2/16_module_demo/app.py
@@ -0,0 +1,10 @@
+from my_calculations import my_add_function as my_add, num1
+
+num2 = 10
+
+result = my_add(num1, num2)
+
+print(f"The sum is {result}")
+
+print(__name__)
+
---
+++ chapter2/16_module_demo/my_calculations.py
@@ -0,0 +1,13 @@
+num1 = 100
+
+def my_add_function(n , m):
+    return n + m
+
+def main():
+    print("Value of num1:", num1)
+    result = my_add_function(50, 75)
+    print(f"The sum is {result}")
+    print(__name__)
+
+if __name__ == "__main__":
+    main()

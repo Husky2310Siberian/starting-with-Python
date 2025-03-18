7b166f461f6c15ccdfed02359166264ac6f636ff
Siberian <vasileios.kritikos@aueb.gr>
2025-03-18 11:33:23 +0200

    updates

--- chapter2/21_var_args.py
+++ chapter2/21_var_args.py
@@ -1,11 +1,20 @@
-def print_cities(*cities):
+def print_cities(*cities , seperator=", " , end="\t") -> str:
+    """
+    Returns a string, representing *cities".
+
+    Args:
+        :param cities: a string of cities
+        :param seperator: the seperator " , "
+        :param end: the end " \t "
+        :return: a string
+    """
     if not cities:
         print("No cities available")
     else:
-        print("Cities:" , " , " .join(cities))
+        print("Cities:", ", " .join(cities), sep=seperator, end=end)
 
 def main():
-    print_cities("Napoli, London, Calgary")
+    print_cities("Napoli", "London", "Calgary" , seperator=" | " , end=".")
     print_cities("Sydney")
     print_cities("")
     print_cities()

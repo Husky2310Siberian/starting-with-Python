96b4b8a0a653c7b1a8aa82724ea94c4d155281fe
Siberian <vasileios.kritikos@aueb.gr>
2025-03-18 10:34:27 +0200

    params demo

--- chapter2/19_demo.py
+++ chapter2/19_demo.py
@@ -1,11 +1,14 @@
 # Falsy - Truthy Values
 # 0 , 0.0 , 0j , "" , [] , {} , set() , False , None
+from numpy.f2py.crackfortran import endpattern
 
 empty_dict = {}
 print(type(empty_dict))
 
-a = 5
+empty_set = set()
+print(type(empty_set))
 
+a = 5
 if a > 0 and a < 10:
     print("Valid number")
 
@@ -17,3 +20,10 @@ students = ("Alice" , "Bob" , "Charlie")
 
 students = tuple(["Siberian"] + list(students)[1:])
 print(students)
+
+print("-----------------------------")
+
+# enumerate()
+students = ("Alice" , "Bob" , "Charlie" , "Siberian")
+for index, value in enumerate(students , start=20):
+     print(f"{index} : {value}")

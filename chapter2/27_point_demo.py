cc8ef084dd109fd41325dbe967a794fbfcf4fa99
Siberian <vasileios.kritikos@aueb.gr>
2025-03-18 15:57:49 +0200

    Update

--- chapter2/27_point_demo.py
+++ chapter2/27_point_demo.py
@@ -1,7 +1,7 @@
 import math
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
@@ -12,6 +12,9 @@ class Point:
         return math.sqrt(math.pow(self.x - other.x , 2) + math.pow(self.y - other.y , 2))
 
 def main():
+    """
+    :return: The Euclidean distance between two points
+    """
 
     p1 = Point(10, 20)
     p2 = Point(20, 30)

9fa013066b9704947831342049d1aa1e4b82abcb
Siberian <vasileios.kritikos@aueb.gr>
2025-03-18 15:55:28 +0200

    Point demo

---
+++ chapter2/27_point_demo.py
@@ -0,0 +1,30 @@
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "Point(x=%d, y=%d)" % (self.x, self.y)
+
+    def distance_to(self , other):
+        return math.sqrt(math.pow(self.x - other.x , 2) + math.pow(self.y - other.y , 2))
+
+def main():
+
+    p1 = Point(10, 20)
+    p2 = Point(20, 30)
+
+    print("Distance between (x,y) is" , p1.distance_to(p2))
+
+    p1.x = 20
+    p1.y = 40
+
+    print("Distance between (x,y) is" , p1.distance_to(p2))
+
+    print("p1:" ,p1.__str__())
+    print("p2:" ,p2.__str__())
+
+if __name__ == "__main__":
+    main()
